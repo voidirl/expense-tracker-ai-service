0b896ff2d3a2e23e5d1ea35c37d2831d4d062d2b
voidirl <rishavrajput204@gmail.com>
2026-03-21 11:14:42 +0530

    dockerize ai service

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://localhost:5173"],
+                   allow_origins=["http://localhost:5173","http://localhost:3000"],
                    allow_methods=["*"],
                    allow_headers=["*"],)
 
