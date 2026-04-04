c649bb08c999d85e1e8a265e182b836eec16317d
voidirl <rishavrajput204@gmail.com>
2026-04-04 11:20:57 +0530

    add vercel domain to cors

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://localhost:5173","http://localhost:3000"],
+                   allow_origins=["http://localhost:5173","http://localhost:3000","https://ai-expense-tracker-frontend-mocha.vercel.app"],
                    allow_methods=["*"],
                    allow_headers=["*"],)
 
